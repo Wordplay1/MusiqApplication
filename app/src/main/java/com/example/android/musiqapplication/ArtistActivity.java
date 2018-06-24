5abdd90ffd831253eea71e62021402771134f7dd
Don Mears <Donaldmears1@yahoo.com>
2018-06-24 17:02:35 -0400

    Added Album, Artist, and Songs activity.

---
+++ app/src/main/java/com/example/android/musiqapplication/ArtistActivity.java
@@ -0,0 +1,12 @@
+package com.example.android.musiqapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ArtistActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.words_list);
+
+    }
--- app/src/main/java/com/example/android/musiqapplication/MainActivity.java
+++ app/src/main/java/com/example/android/musiqapplication/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.musiqapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView songs = (TextView) findViewById(R.id.songs);
+        TextView albums = (TextView) findViewById(R.id.albums);
+        TextView artist = (TextView) findViewById(R.id.artist);
+
+        /**
+         * clickListener for songs TextView
+         */
+        songs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent songsIntent = new Intent(MainActivity.this, SongsActivity.class);
+                startActivity(songsIntent);
+            }
+        });
+        /**
+         * clickListener for albums TextView
+         */
+        albums.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent albumIntent = new Intent(MainActivity.this, AlbumActivity.class);
+                startActivity(albumIntent);
+            }
+        });
+        /**
+         * clickListener for artist TextView
+         */
+        artist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent artistIntent = new Intent(MainActivity.this, ArtistActivity.class);
+                startActivity(artistIntent);
+            }
+        });
     }
 }
